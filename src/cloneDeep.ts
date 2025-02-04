06ea1eaad9f39233be0033a30bc309941118a00b
JaeBeen95 <nin2348@gmail.com>
2025-02-04 19:03:46 +0900

    typescript로 마이그레이션

--- src/cloneDeep.ts
+++ src/cloneDeep.ts
@@ -1,40 +1,3 @@
-function User(username) {
-  this.username = username;
-}
-
-User.prototype.sayHi = function (username) {
-  return `안녕하세요, ${username}입니다`;
-};
-
-const testObject = {
-  user: new User("nin2348"),
-  profile: {
-    hobbies: ["노래", "운동"],
-    info: {
-      age: 30,
-      city: "why",
-    },
-  },
-
-  get greet() {
-    return "Hello";
-  },
-};
-
-const testArray = [
-  1,
-  "문자열",
-  [1, 2, 3],
-  new User("nin2348"),
-  {
-    deep: {
-      deeper: {
-        deepest: "가장 깊은 값",
-      },
-    },
-  },
-];
-
 function cloneDeep(target) {
   if (typeof target !== "object" || target === null) {
     return target;
@@ -64,5 +27,3 @@ function cloneDeep(target) {
 
   return clonedObject;
 }
-
-console.log(testObject, cloneDeep(testObject));
