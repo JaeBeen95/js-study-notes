1f92af8bcea0c46624028396ce71a6a4796a209a
JaeBeen95 <nin2348@gmail.com>
2025-02-18 17:15:19 +0900

    reject 함수 구현

--- src/customPromise.js
+++ src/customPromise.js
@@ -6,13 +6,18 @@ class CustomPromise {
     executor(this.resolve.bind(this), this.reject.bind(this));
   }
 
-  resolve(result) {
+  resolve(value) {
     if (this.promiseState !== "pending") return;
-    this.promiseResult = result;
+    this.promiseResult = value;
     this.promiseState = "fulfilled";
   }
 
-  reject() {}
+  reject(error) {
+    if (this.promiseState !== "pending") return;
+    this.promiseResult = error;
+    this.promiseState = "rejected";
+    throw new Error("Uncaught (in promise) " + error);
+  }
 
   then() {}
 
@@ -20,12 +25,3 @@ class CustomPromise {
 
   finally() {}
 }
-
-const promise = new Promise((resolve) => resolve(1));
-
-console.log(
-  new CustomPromise((resolve) => {
-    resolve("1");
-  }),
-  promise
-);
